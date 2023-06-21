e27379da0af6697fec2abc315b0b2c4603366a65
jonpfluger <jonpfluger718@gmail.com>
2023-06-20 21:11:13 -0500

    add click listener to start button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,46 @@
 // select dom elements
-  var startButton = document.querySelector(".start-button")
-  var resetButton = document.querySelector(".reset-button")
-  var timerEl = document.querySelector(".timer")
-  var wordBlanksEl = document.querySelector(".word-blanks")
-  var winEl = document.querySelector(".win")
-  var lossesEl = document.querySelector(".lose")
+var startButton = document.querySelector(".start-button")
+var resetButton = document.querySelector(".reset-button")
+var timerEl = document.querySelector(".timer")
+var wordBlanksEl = document.querySelector(".word-blanks")
+var winEl = document.querySelector(".win")
+var lossesEl = document.querySelector(".lose")
 
-  // list of words
-  var words = ["Jellyfish", "pancakes", "callback", "bacon", "porcupine"]
-  var userGuesses = []
-  var answer
+// list of words
+var words = ["Jellyfish", "pancakes", "callback", "bacon", "porcupine"]
+var userGuesses = []
+var answer
 
-  function startRound() {
-    // start timer
-    // pick a random word
-    // set word-blanks to have _'s
-  }
+function startRound() {
+  // start timer
+  // pick a random word
+  // set word-blanks to have _'s
+}
 
-  function onKeydown(event) {
-    // check if letter pressed is in the answer string
-      // if yes
-        // reveal letter in word-blanks
-  }
+function onKeydown(event) {
+  // check if letter pressed is in the answer string
+    // if yes
+      // reveal letter in word-blanks
+}
 
-  function tick() {
-    // check if userGuesses has all letters that are in answer string
-      // if has guess all letters
-        // end round
-        // increment wins
-        // display "you won!"
-      // else
-        // is our timer count at 0?
-          // if yes
-            // diplay "lost round"
-            // increment losses
-          // else
-            // decrement timer count
-  }
+function tick() {
+  // check if userGuesses has all letters that are in answer string
+    // if has guess all letters
+      // end round
+      // increment wins
+      // display "you won!"
+    // else
+      // is our timer count at 0?
+        // if yes
+          // diplay "lost round"
+          // increment losses
+        // else
+          // decrement timer count
+}
 
-  // Event Listeners
-    // start - startRound()
-    // reset
-    // keydown - onKeydown
+// Event Listeners
+  // start - startRound()
+  // reset
+  // keydown - onKeydown
+
+startButton.addEventListener("click", startRound)
